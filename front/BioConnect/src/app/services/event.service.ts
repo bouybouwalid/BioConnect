e7cfc9548e5f1918295c1ddb262334fd8143b133
bouybouwalid <walidbouybou@gmail.com>
2025-02-01 23:10:49 +0100

    Ajout des modifications 31/01/2025

---
+++ front/BioConnect/src/app/services/event.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Event {
+  id: number;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  image: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+  private apiUrl = 'http://localhost:8080/events';
+
+  constructor(private http: HttpClient) {}
+
+  getEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(this.apiUrl);
+  }
+}
